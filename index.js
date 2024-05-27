c5688a0ae9629cf2e7abb4873d7f34934c93b1ce
poju <rasmus.hintzmadsen@gmail.com>
2024-05-27 16:53:46 +0300

    osa 3c done

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const Person = require("./models/person");
 
 app.use(cors());
-
 app.use(express.static("dist"));
+app.use(express.json());
 
 morgan.token("body", (req, res) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -23,23 +23,29 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", async (request, response) => {
-  const id = request.params.id;
-  const person = await Person.findById(id);
-  if (!person) {
-    response.status(404).send("Not found");
-    return;
+app.get("/api/persons/:id", async (request, response, next) => {
+  try {
+    const id = request.params.id;
+    const person = await Person.findById(id);
+    if (!person) {
+      response.status(404).end();
+      return;
+    }
+    await response.json(person);
+  } catch (e) {
+    next(e);
   }
-  await response.json(person);
 });
 
-app.delete("/api/persons/:id", async (request, response) => {
-  const id = request.params.id;
-  persons = await Person.findByIdAndDelete(id);
-  response.status(204).send();
+app.delete("/api/persons/:id", async (request, response, next) => {
+  try {
+    const id = request.params.id;
+    persons = await Person.findByIdAndDelete(id);
+    response.status(204).send();
+  } catch (e) {
+    next(e);
+  }
 });
-
-app.use(express.json());
 
 app.post("/api/persons", async (request, response) => {
   const body = request.body;
@@ -64,11 +70,44 @@ app.post("/api/persons", async (request, response) => {
   response.json(savedPerson);
 });
 
+app.put("/api/persons/:id", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const id = req.params.id;
+
+    console.log("updated person:", body);
+
+    const updatedPerson = await Person.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    res.json(updatedPerson);
+  } catch (e) {
+    next(e);
+  }
+});
+
 app.get("/info", async (request, response) => {
   const length = await Person.countDocuments({});
   response.send(`<p>Phonebook has ${length} people</p>
     <p>${new Date()}</p>`);
 });
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(400).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
